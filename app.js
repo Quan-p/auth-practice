f2b483ee7d65ed1bf869a9a44b7ab3f502feb6ca
quan-p <m.phamquan@gmail.com>
2022-07-12 18:00:59 -0500

    Functions to create session in cookie

--- app.js
+++ app.js
@@ -38,6 +38,16 @@ passport.use(
     })
 );
 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+});
+
 const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
